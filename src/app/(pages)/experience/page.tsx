b2a95b7856ae60535d9256155afb1a5d6a4dc36e
Matt Ondo <yours.truly@mattondo.com>
2025-04-20 12:59:08 -0600

    fix line break on mobile

--- src/app/(pages)/experience/page.tsx
+++ src/app/(pages)/experience/page.tsx
@@ -26,7 +26,7 @@ export default async function Page() {
                 <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
               </svg>
               <div className="ml-6">
-                <h4 className="font-bold"><span className="text-xl">{entry.title}</span> <span className="sr-only">at</span> <span className="text-purple not-sr-only">@</span> <span className="font-light">{entry.company_name}</span></h4>
+                <h4 className="font-bold"><span className="text-xl">{entry.title}</span> <span className="sr-only">at</span>&nbsp;<span className="text-purple not-sr-only">@</span>&nbsp;<span className="font-light">{entry.company_name}</span></h4>
                 <p className="mt-1 block text-sm font-light">{entry.start_date} - {entry.end_date}</p>
                 <div className="mt-2 prose-sm prose dark:prose-invert" dangerouslySetInnerHTML={{__html: entry.overview}} />
               </div>
