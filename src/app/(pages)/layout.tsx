7ddee526075e83d26b63a82dc6ff9a60cb6fd78c
Matt Ondo <yours.truly@mattondo.com>
2025-05-18 08:49:37 -0600

    update semantics, use site header component

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,4 @@
-import { AveryOndoLogo } from "@/components/logos"
-import Link from "next/link"
-import { Navigation } from "@/components/navigation"
+import { SiteHeader } from "@/components/header"
 
 export default function PagesLayout({
   children,
@@ -9,15 +7,10 @@ export default function PagesLayout({
 }>) {
   return (
     <>
-      <div className="h-auto w-full md:h-dvh md:self-start md:sticky md:top-0 md:w-sm flex flex-col items-center justify-center">
-        <Link href="/" className="w-full h-auto max-w-3xs mt-10 md:mt-0 mb-8">
-          <AveryOndoLogo className="w-full h-full" />
-        </Link>
-        <Navigation />
-      </div>
-      <div id="main-content" className="w-full h-full flex flex-col justify-center mt-8 md:mt-0 overflow-y-auto">
+      <SiteHeader />
+      <main id="main-content" className="w-full h-full flex flex-col justify-center mt-8 md:mt-0 overflow-y-auto">
         {children}
-      </div>
+      </main>
     </>
   );
 }
