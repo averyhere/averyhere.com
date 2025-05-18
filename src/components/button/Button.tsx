816084bbddad8958d59ec2757232f5b06da7485e
Matt Ondo <yours.truly@mattondo.com>
2025-05-18 10:33:20 -0600

    add button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,42 @@
+import Link, { LinkProps } from "next/link"
+import { IconType } from "react-icons"
+import styles from "./button.module.css"
+
+interface ButtonProps extends LinkProps {
+  className?: string,
+  children?: React.ReactNode,
+  external?: boolean,
+  size?: 'sm'|'md'|'lg',
+  icon?: IconType,
+  iconPosition?: 'start'|'end',
+}
+
+export const Button: React.FC<ButtonProps> = ({ 
+  children, 
+  href, 
+  external, 
+  size = 'md',
+  icon, 
+  iconPosition = 'start', 
+  className, 
+  ...props 
+}) => {
+  
+  if (icon){
+    const Icon:IconType = icon;
+    let sizeClass = size === 'md' ? 'size-8' : size === 'sm' ? 'size-4' : 'size-10';
+    return (
+      <Link href={href} className={`${styles.button} ${size !== 'md' ? styles[`button-${size}`] : ''}`} {... (external ? { target: "_blank" } : {} )} {...props}>
+        {iconPosition === 'start' && (<Icon className={sizeClass} />)} 
+        {children}
+        {iconPosition === 'end' && (<Icon className={sizeClass} />)} 
+      </Link>
+    )
+  }
+
+  return (
+    <Link href={href} className={styles.button} {... (external ? { target: "_blank" } : {} )} {...props}>
+      {children}
+    </Link>
+  )
+}
