a93e1d2856023b463298adab691012ce17da26e8
Matt Ondo <yours.truly@mattondo.com>
2025-05-18 20:57:02 -0600

    add link to resume

--- src/app/(pages)/experience/page.tsx
+++ src/app/(pages)/experience/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from '@utils/supabase/server';
 import type { Metadata } from 'next'
+import Link from "next/link";
 
 export const dynamic = 'force-static';
  
@@ -18,6 +19,7 @@ export default async function Page() {
         <h1 className="text-3xl font-bold">
           Work Experience
         </h1>
+        <p><small><Link href="/resume/" rel="nofollow" className='text-bright-purple dark:text-pink underline-offset-4 decoration-blue hover:underline'>View printable resume</Link></small></p>
       </header>
 
       <div className="flex items-center justify-center pl-3">
