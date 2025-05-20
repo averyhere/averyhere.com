2d9000cbd8f067c03a19855afc68e7a65f31121f
Matt Ondo <yours.truly@mattondo.com>
2025-05-20 11:56:16 -0600

    allow for link and button styles

--- src/components/auth/LogoutButton.tsx
+++ src/components/auth/LogoutButton.tsx
@@ -1,11 +1,12 @@
 import { logout } from './actions'
 import buttonStyles from "@/components/button/button.module.css"
 
-export const LogoutButton = () => {
+export const LogoutButton = ({variant = 'button'}: { variant?: 'button'|'link' }) => {
   
   return (
     <form className="inline">
-      <button type="submit" className={`${buttonStyles.button} ${buttonStyles['button-sm']}`} formAction={logout}>Logout</button>
+      {variant === 'button' && (<button type="submit" className={`${buttonStyles.button} ${buttonStyles['button-sm']}`} formAction={logout}>Logout</button>)}
+      {variant === 'link' && (<button type="submit" className='nav-item cursor-pointer text-sm hover:underline' formAction={logout}>logout</button>)}
     </form>
   )
 }
