e6466a715b39630b161645803cf5ad13ae99a512
Matt Ondo <yours.truly@mattondo.com>
2025-05-20 12:57:05 -0600

    add list pages

---
+++ src/app/admin/list/[slug]/page.tsx
@@ -0,0 +1,128 @@
+import type { Metadata } from 'next'
+import { createClient } from '@/utils/supabase/server'
+import { AdminNav } from '@/components/AdminNav'
+import { redirect } from 'next/navigation'
+import Link from 'next/link'
+import { PiPencilSimpleDuotone, PiPlusDuotone } from "react-icons/pi";
+
+export const metadata: Metadata = {
+  title: 'Avery Ondo',
+  description: 'Avery Ondo\'s personal website.',
+}
+
+export default async function PrivatePage({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+  const supabase = await createClient()
+  const { data: authData, error: authError } = await supabase.auth.getUser()
+  
+  if (authError || !authData?.user) {
+    redirect('/admin/')
+  }
+  
+  if (slug === 'projects') {
+    const { data: projects } = await supabase.from("projects").select();
+
+    return (
+      <>
+        <AdminNav />
+        <ProjectList projects={projects} />
+      </>
+    )
+
+  }
+
+  if (slug === 'experience') {
+    const { data: experience } = await supabase.from("work_experience").select();
+
+    return (
+      <>
+        <AdminNav />
+        <ExperienceList data={experience} />
+      </>
+    )
+  }
+  
+  return (
+    <>
+      <AdminNav />
+    </>
+  )
+}
+
+const ProjectList = ({projects}: {projects:any}) => {
+
+  return (
+
+    <section className="flex flex-col">
+      <h2 className="text-3xl font-bold">All Projects</h2>
+
+      <div className="divide-y divide-purple divide-dashed">
+        {projects?.map((entry: any) => (
+          <article className="py-4 relative flex justify-between" key={entry.id}>
+            <div>
+              <h2 className="font-bold text-xl mr-2">{entry.title}</h2>
+              <span className="inline text-purple font-bold">/&zwj;/</span>&nbsp;<span className="inline font-light">{entry.subtitle}</span>
+            </div>
+            <Link
+                href={`/admin/edit/project/${entry.id}/`}
+                className="flex items-center gap-0.5 text-bright-purple dark:text-pink text-sm hover:underline decoration-blue underline-offset-4"
+                >
+                  <PiPencilSimpleDuotone className="size-3" />
+                  Edit
+              </Link>
+          </article>
+        ))}
+        <article className="py-4 relative flex justify-end">
+          <Link
+              href={`/admin/create/project/`}
+              className="flex items-center gap-2 text-bright-purple dark:text-pink text-lg hover:underline decoration-blue underline-offset-4"
+              >
+                <PiPlusDuotone className="size-8" />
+                Add Project
+            </Link>
+        </article>
+      </div>
+    </section>
+  )
+}
+
+const ExperienceList = ({data}: {data:any}) => {
+
+  return (
+
+    <section className="flex flex-col">
+      <h2 className="text-3xl font-bold">Work Experience</h2>
+
+      <div className="divide-y divide-purple divide-dashed">
+        {data?.map((entry: any) => (
+          <article className="py-4 relative flex justify-between" key={entry.id}>
+            <div>
+              <h2 className="font-bold text-xl mr-2">{entry.title}</h2>
+              <span className="inline text-purple font-bold">/&zwj;/</span>&nbsp;<span className="inline font-light">{entry.company_name}</span>
+            </div>
+            <Link
+                href={`/admin/edit/experience/${entry.id}/`}
+                className="flex items-center gap-0.5 text-bright-purple dark:text-pink text-sm hover:underline decoration-blue underline-offset-4"
+                >
+                  <PiPencilSimpleDuotone className="size-3" />
+                  Edit
+              </Link>
+          </article>
+        ))}
+        <article className="py-4 relative flex justify-end">
+          <Link
+              href={`/admin/create/experience/`}
+              className="flex items-center gap-2 text-bright-purple dark:text-pink text-lg hover:underline decoration-blue underline-offset-4"
+              >
+                <PiPlusDuotone className="size-8" />
+                Add Experience
+            </Link>
+        </article>
+      </div>
+    </section>
+  )
+}
--- src/components/AdminNav.tsx
+++ src/components/AdminNav.tsx
@@ -12,8 +12,8 @@ export const AdminNav = () => {
     <nav className='flex flex-wrap gap-4 justify-between align'>
       <ul className='flex flex-wrap gap-4 mt-1'>
         <li><Link href="/admin/" className={`flex items-center gap-1 text-bright-purple dark:text-pink underline-offset-4 decoration-blue hover:underline ${pathname === '/admin/' ? 'underline' : ''}`}>Dashboard</Link></li>
-        <li><Link href="/admin/create/experience/" className={`flex items-center gap-1 text-bright-purple dark:text-pink underline-offset-4 decoration-blue hover:underline ${pathname === '' ? 'underline' : ''}`}><PiPlusDuotone /> Add Experience</Link></li>
-        <li><Link href="/admin/create/project/" className={`flex items-center gap-1 text-bright-purple dark:text-pink underline-offset-4 decoration-blue hover:underline ${pathname === '' ? 'underline' : ''}`}><PiPlusDuotone /> Add Project</Link></li>
+        <li><Link href="/admin/list/experience/" className={`flex items-center gap-1 text-bright-purple dark:text-pink underline-offset-4 decoration-blue hover:underline ${pathname.includes('/experience/') ? 'underline' : ''}`}>Experience</Link></li>
+        <li><Link href="/admin/list/projects/" className={`flex items-center gap-1 text-bright-purple dark:text-pink underline-offset-4 decoration-blue hover:underline ${pathname.includes('/project/') || pathname.includes('/projects/') ? 'underline' : ''}`}>Projects</Link></li>
         <li><Link href="/admin/profile/" className={`flex items-center gap-1 text-bright-purple dark:text-pink underline-offset-4 decoration-blue hover:underline ${pathname === '/admin/profile/' ? 'underline' : ''}`}><PiUserCircleGearDuotone /> Edit Profile</Link></li>
       </ul>
       <LogoutButton />
