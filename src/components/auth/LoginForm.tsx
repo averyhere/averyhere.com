6e130e93caa1ae6b5e6ac1e5ec1f6b4c0d6f0485
Matt Ondo <yours.truly@mattondo.com>
2025-05-20 12:04:00 -0600

    add headline for login

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -5,6 +5,7 @@ export const LoginForm = () => {
   
   return (
     <form className="grid grid-col-1 gap-4 w-full max-w-xs mx-auto">
+      <h1 className='text-3xl text-center font-bold'>Please login</h1>
       <div className="flex flex-col">
         <label>Email Address</label>
         <input name="email" type="email" className="bg-purple/20 p-2" required />
