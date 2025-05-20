adcae65f77dda5f2acaa1081f88a4d48659059ff
Matt Ondo <yours.truly@mattondo.com>
2025-05-20 11:55:25 -0600

    add xs size

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps extends LinkProps {
   className?: string,
   children?: React.ReactNode,
   external?: boolean,
-  size?: 'sm'|'md'|'lg',
+  size?: 'xs'|'sm'|'md'|'lg',
   icon?: IconType,
   iconPosition?: 'start'|'end',
 }
@@ -24,9 +24,9 @@ export const Button: React.FC<ButtonProps> = ({
   
   if (icon){
     const Icon:IconType = icon;
-    const sizeClass = size === 'md' ? 'size-8' : size === 'sm' ? 'size-4' : 'size-10';
+    const sizeClass = size === 'md' ? 'size-8' : size === 'sm' ? 'size-4' : size === 'xs' ? 'size-3' : 'size-10';
     return (
-      <Link href={href} className={`${styles.button} ${size !== 'md' ? styles[`button-${size}`] : ''} ${className}`} {... (external ? { target: "_blank" } : {} )} {...props}>
+      <Link href={href} className={`${styles.button} ${size !== 'md' ? styles[`button-${size}`] : ''} ${styles[`button-icon-${iconPosition}`]} ${className}`} {... (external ? { target: "_blank" } : {} )} {...props}>
         {iconPosition === 'start' && (<Icon className={sizeClass} />)} 
         {children}
         {iconPosition === 'end' && (<Icon className={sizeClass} />)} 
@@ -35,7 +35,7 @@ export const Button: React.FC<ButtonProps> = ({
   }
 
   return (
-    <Link href={href} className={styles.button} {... (external ? { target: "_blank" } : {} )} {...props}>
+    <Link href={href} className={`${styles.button} ${size !== 'md' ? styles[`button-${size}`] : ''} ${className}`} {... (external ? { target: "_blank" } : {} )} {...props}>
       {children}
     </Link>
   )
