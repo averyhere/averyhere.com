97790208d2bb6ee03d1ad08d9e3972d60aa18d04
Matt Ondo <yours.truly@mattondo.com>
2025-05-21 11:06:22 -0600

    new components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () => {
+  return (
+    <footer><p className="text-center grow-0 print:hidden my-4"><small>© Avery Ondo</small></p></footer>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
+import { Accordion } from './Accordion'
 import { AdminNav } from './AdminNav'
 import { Button } from './button'
+import { Footer } from './Footer'
 import { MainNav } from './MainNav'
 import RichTextEditor from './RichTextEditor'
 import { SiteHeader } from './SiteHeader'
@@ -12,8 +14,10 @@ import {
 } from './logos'
 
 export {
+  Accordion,
   AdminNav,
   Button,
+  Footer,
   MainNav,
   RichTextEditor,
   SiteHeader,
