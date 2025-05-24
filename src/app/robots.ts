dd71e4c419af24cdd22809dd119f55c5c3b4ffcc
Matt Ondo <yours.truly@mattondo.com>
2025-05-24 09:53:05 -0600

    updates

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/resume/','/admin/'],
+      disallow: ['/admin/','/login/','/verify-mfa/','/error/'],
     },
     sitemap: 'https://averyhere.com/sitemap.xml',
   }
