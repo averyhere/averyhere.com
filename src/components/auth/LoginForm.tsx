868b8a1b070e2d14fbe26f07049fcb4b00a70313
Matt Ondo <yours.truly@mattondo.com>
2025-05-24 08:33:56 -0600

    update input styles for consistency

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -20,11 +20,11 @@ export const LoginForm = () => {
       <form className="grid grid-col-1 gap-4 w-full max-w-xs mx-auto" action={handleSubmit}>
         <div className="flex flex-col">
           <label>Email Address</label>
-          <input name="email" type="email" className="bg-purple/20 p-2" required />
+          <input name="email" type="email" className="bg-purple/20 p-2 border border-purple" required />
         </div>
         <div className="flex flex-col">
           <label>Password</label>
-          <input name="password" type="password" className="bg-purple/20 p-2" required />
+          <input name="password" type="password" className="bg-purple/20 p-2 border border-purple" required />
         </div>
         <button type="submit" className={`${buttonStyles.button} !justify-center !w-full`}>Login</button>
       </form>
