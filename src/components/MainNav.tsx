47708d7b591335da4d41f7a83ea69af8654320a9
Matt Ondo <yours.truly@mattondo.com>
2025-05-25 08:31:58 -0600

    use navlink

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,25 +1,21 @@
-"use client"
-
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { NavLink } from '@/components/ui'
 
 export const MainNav = ({ variant }: { variant?: string }) => {
-  const pathname = usePathname();
   const classes = variant === 'horizontal' ? 'grid grid-cols-1 md:grid-cols-4 max-w-max m-auto gap-4' : 'flex flex-row flex-wrap gap-4 m-auto justify-center'
   return (
     <nav className="w-full text-center text-md md:text-xl">
       <ul className={classes}>
         <li className="w-auto md:w-full">
-          <Link href="/about/" className={`decoration-blue underline-offset-4 hover:underline ${pathname === '/about/' ? 'underline' : ''}`}>about</Link>
+          <NavLink href="/about/" className='decoration-blue underline-offset-4 hover:underline' activeClasses='underline'>about</NavLink>
         </li>
         <li className="w-auto md:w-full">
-          <Link href="/experience/" className={`decoration-blue underline-offset-4 hover:underline ${pathname === '/experience/' ? 'underline' : ''}`}>experience</Link>
+          <NavLink href="/experience/" className='decoration-blue underline-offset-4 hover:underline' activeClasses='underline'>experience</NavLink>
         </li>
         <li className="w-auto md:w-full">
-          <Link href="/projects/" className={`decoration-blue underline-offset-4 hover:underline ${pathname === '/projects/' ? 'underline' : ''}`}>projects</Link>
+          <NavLink href="/projects/" className='decoration-blue underline-offset-4 hover:underline' activeClasses='underline'>projects</NavLink>
         </li>
         <li className="w-auto md:w-full">
-          <Link href="/contact/" className={`decoration-blue underline-offset-4 hover:underline ${pathname === '/contact/' ? 'underline' : ''}`}>contact</Link>
+          <NavLink href="/contact/" className='decoration-blue underline-offset-4 hover:underline' activeClasses='underline'>contact</NavLink>
         </li>
       </ul>
     </nav>
--- src/components/admin/AdminNav.tsx
+++ src/components/admin/AdminNav.tsx
@@ -1,20 +1,22 @@
-"use client"
-
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { LogoutButton } from '@/components/auth'
 import { PiGearSixDuotone, PiSpeedometerDuotone, PiPuzzlePieceDuotone, PiBuildingOfficeDuotone } from "react-icons/pi";
+import { createClient } from '@/utils/supabase/server'
+import { NavLink } from '@/components/ui'
 
-export const AdminNav = () => {
-  const pathname = usePathname();
+export const AdminNav = async () => {
+  const supabase = await createClient()
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    return null;
+  }
 
   return (
     <nav className='w-full flex items-center flex-wrap gap-4 justify-between align text-xs'>
       <ul className='flex flex-wrap gap-4 mt-1 justify-center'>
-        <li><Link href="/admin/" className={`flex items-center gap-1 text-bright-purple dark:text-pink border-0 border-b-4 ${pathname === '/admin/' ? 'border-blue' : 'border-transparent'}`}><PiSpeedometerDuotone className="size-6" /> Dashboard</Link></li>
-        <li><Link href="/admin/list/experience/" className={`flex items-center gap-1 text-bright-purple dark:text-pink border-0 border-b-4 ${pathname.includes('/experience/') ? 'border-blue' : 'border-transparent'}`}><PiBuildingOfficeDuotone className="size-6" /> Experience</Link></li>
-        <li><Link href="/admin/list/projects/" className={`flex items-center gap-1 text-bright-purple dark:text-pink border-0 border-b-4 ${pathname.includes('/project/') || pathname.includes('/projects/') ? 'border-blue' : 'border-transparent'}`}><PiPuzzlePieceDuotone className="size-6" /> Projects</Link></li>
-        <li><Link href="/admin/profile/" className={`flex items-center gap-1 text-bright-purple dark:text-pink border-0 border-b-4 mb-1.5 ${pathname === '/admin/profile/' ? 'border-blue' : 'border-transparent'}`} title="Edit profile"><PiGearSixDuotone className="size-6" /> Settings</Link></li>
+        <li><NavLink href="/admin/" className='flex items-center gap-1 text-bright-purple dark:text-pink border-0 border-b-4 border-transparent' activeClasses='border-blue'><PiSpeedometerDuotone className="size-6" /> Dashboard</NavLink></li>
+        <li><NavLink href="/admin/list/experience/" className='flex items-center gap-1 text-bright-purple dark:text-pink border-0 border-b-4 border-transparent' activeClasses='border-blue'><PiBuildingOfficeDuotone className="size-6" /> Experience</NavLink></li>
+        <li><NavLink href="/admin/list/projects/" className='flex items-center gap-1 text-bright-purple dark:text-pink border-0 border-b-4 border-transparent' activeClasses='border-blue'><PiPuzzlePieceDuotone className="size-6" /> Projects</NavLink></li>
+        <li><NavLink href="/admin/profile/" className='flex items-center gap-1 text-bright-purple dark:text-pink border-0 border-b-4 mb-1.5 border-transparent' activeClasses='border-blue'><PiGearSixDuotone className="size-6" /> Settings</NavLink></li>
       </ul>
       <ul className='flex flex-wrap gap-4 mt-1 justify-center'>
         <li><LogoutButton /></li>
