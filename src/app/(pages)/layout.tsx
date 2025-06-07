1652236d0353d6f34b9e63062fd27b00c8052897
Matt Ondo <yours.truly@mattondo.com>
2025-06-07 08:58:15 -0600

    Move footer in DOM

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -7,16 +7,18 @@ export default function PagesLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-dvh flex flex-col justify-between">
-      <div className="grid gap-4 md:grid-cols-12 max-w-4xl mx-auto px-4">
-        <SiteHeader className="md:col-span-4" />
-        <div className="md:col-start-5 md:col-span-7 flex flex-col justify-center">
-          <main id="main-content" className="flex flex-col h-full justify-center">
-            {children}
-          </main>
-          <Footer />
+    <>
+      <div className="min-h-dvh flex flex-col justify-between">
+        <div className="grid gap-4 md:grid-cols-12 max-w-4xl mx-auto px-4">
+          <SiteHeader className="md:col-span-4" />
+          <div className="md:col-start-5 md:col-span-7 flex flex-col justify-center">
+            <main id="main-content" className="flex flex-col h-full justify-center">
+              {children}
+            </main>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
