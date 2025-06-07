2df81f3e3ad386574f0fc8fdf883ec6ff06555a8
Matt Ondo <yours.truly@mattondo.com>
2025-06-07 07:58:04 -0600

    fix urls & add pages

--- src/app/(auth)/admin/page.tsx
+++ src/app/(auth)/admin/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from "next/navigation";
 import { Button } from '@/components/ui'
-import { PiPlusDuotone, PiPuzzlePieceDuotone, PiUserDuotone, PiGearSixDuotone, PiBuildingOfficeDuotone, PiListDuotone } from "react-icons/pi";
+import { PiPlusDuotone, PiPuzzlePieceDuotone, PiArticleDuotone, PiUserDuotone, PiGearSixDuotone, PiBuildingOfficeDuotone, PiListDuotone } from "react-icons/pi";
 import Link from 'next/link'
 
 export const metadata: Metadata = {
@@ -27,27 +27,37 @@ export default async function PrivatePage() {
 
       <div className='grid gap-4'>
         <div className='flex justify-between items-center'>
-          <Link href="/admin/list/experience/" className='flex gap-2 items-center'>
+          <Link href="/admin/experience/" className='flex gap-2 items-center'>
             <PiBuildingOfficeDuotone className='size-14' />
             <h2 className='text-xl'>Experience</h2>
           </Link>
           <div className='flex flex-wrap mt-2'>
-            <Button href="/admin/list/experience/" size="sm" icon={PiListDuotone}>View All</Button>
-            <Button href="/admin/create/experience/" size="sm" icon={PiPlusDuotone}>Add New</Button>
+            <Button href="/admin/experience/" size="sm" icon={PiListDuotone}>View All</Button>
+            <Button href="/admin/experience/new/" size="sm" icon={PiPlusDuotone}>Add New</Button>
           </div>
         </div>
         <div className='flex justify-between items-center'>
-          <Link href="/admin/list/projects/" className='flex gap-2 items-center'>
+          <Link href="/admin/pages/" className='flex gap-2 items-center'>
+            <PiArticleDuotone className='size-14' />
+            <h2 className='text-xl'>Pages</h2>
+          </Link>
+          <div className='flex flex-wrap mt-2'>
+            <Button href="/admin/pages/" size="sm" icon={PiListDuotone}>View All</Button>
+            <Button href="/admin/pages/new/" size="sm" icon={PiPlusDuotone}>Add New</Button>
+          </div>
+        </div>
+        <div className='flex justify-between items-center'>
+          <Link href="/admin/projects/" className='flex gap-2 items-center'>
             <PiPuzzlePieceDuotone className='size-14' />
             <h2 className='text-xl'>Projects</h2>
           </Link>
           <div className='flex flex-wrap mt-2'>
-            <Button href="/admin/list/projects/" size="sm" icon={PiListDuotone}>View All</Button>
-            <Button href="/admin/create/project/" size="sm" icon={PiPlusDuotone}>Add New</Button>
+            <Button href="/admin/projects/" size="sm" icon={PiListDuotone}>View All</Button>
+            <Button href="/admin/projects/new/" size="sm" icon={PiPlusDuotone}>Add New</Button>
           </div>
         </div>
         <div className='flex justify-between items-center'>
-          <Link href="/admin/list/projects/" className='flex gap-2 items-center'>
+          <Link href="/admin/profile/" className='flex gap-2 items-center'>
             <PiUserDuotone className='size-14' />
             <h2 className='text-xl'>My Profile</h2>
           </Link>
