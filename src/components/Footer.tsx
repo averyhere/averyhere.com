020412ffa3d1fc2fbcbde18029b008ba4cf1c439
Matt Ondo <yours.truly@mattondo.com>
2025-06-07 08:58:00 -0600

    add pride flags to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 import { createClient } from '@/utils/supabase/server'
 import Link from "next/link"
 import { LogoutButton } from '@/components/auth'
+import Image from "next/image"
 
 export const Footer = async () => {
   const supabase = await createClient()
   const { data, error } = await supabase.auth.getUser()
   return (
-    <footer className="justify-self-end w-full">
+    <footer className="justify-self-end w-full mt-10">
       <div className="text-center grow-0 print:hidden my-1 flex flex-col items-center justify-center gap-1 text-xs">
         {(!error && data?.user) && (
           <span>
@@ -15,6 +16,24 @@ export const Footer = async () => {
             <LogoutButton variant='link' />
           </span>
         )}
+
+        <div className='flex gap-1'>
+          <Image
+            src="/images/rainbow-flag-quasar-progress.svg"
+            width={512}
+            height={325}
+            className='h-8 w-auto'
+            alt="LGBTQIA+ Rainbow Flag Quasar Progress Variant"
+          />
+          <Image
+            src="/images/transgender-pride-flag.svg"
+            width={800}
+            height={480}
+            className='h-8 w-auto'
+            alt="LGBTQIA+ Rainbow Flag Quasar Progress Variant"
+          />
+        </div>
+
         <span>© Avery Ondo</span>
       </div>
     </footer>
