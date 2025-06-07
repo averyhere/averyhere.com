9cfbccc91bce82c9d6ed2f864396dfa1f292e645
Matt Ondo <yours.truly@mattondo.com>
2025-06-07 08:58:30 -0600

    add quote from Sarah McBride to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,20 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <main id="main-content" className="h-full w-full flex flex-col min-h-dvh gap-4 items-center justify-center p-4">
+      <main id="main-content" className="h-full w-full flex flex-col min-h-dvh gap-4 items-center justify-center p-4 relative">
         <Link href="/" className="w-full h-auto max-w-lg">
           <AveryOndoLogo className="w-full h-full" />
           <h1 className="sr-only">Avery Ondo</h1>
         </Link>
         <p className="text-bright-purple dark:text-pink">adventurer, web developer, marketer</p>
         <MainNav variant="horizontal" />
+
+        <div className="absolute bottom-0 w-full max-w-max text-sm text-center">
+          <blockquote className='footer-quote'>
+            <p className="text-pretty">We must never be a country that says there’s only one way to love, only one way to look, and only one way to live.</p>
+            <cite className="text-bright-purple dark:text-pink"><strong>Sarah McBride</strong></cite>
+          </blockquote>
+        </div>
       </main>
 
       <Footer />
