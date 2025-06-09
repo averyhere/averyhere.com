5f5ee642de21c5f39072da23c5f937a811fc3140
Matt Ondo <yours.truly@mattondo.com>
2025-06-09 07:32:44 -0600

    update disallow urls based on recent updates

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/admin/','/login/','/verify-mfa/','/error/'],
+      disallow: ['/admin/','/login/','/login/mfa/','/error/'],
     },
     sitemap: 'https://averyhere.com/sitemap.xml',
   }
