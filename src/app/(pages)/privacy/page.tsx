083a0ee674c923a2616d267b65b1cc8cdc84524c
Avery Ondo <hello@averyhere.com>
2025-07-20 14:56:15 -0600

    add privacy page

---
+++ src/app/(pages)/privacy/page.tsx
@@ -0,0 +1,11 @@
+export default async function Page() {
+  
+  return (
+    <div className="prose dark:prose-invert">
+      <p>Thank you for visiting our website and using our app. Your privacy is important to us.</p>
+      <p>We want to assure you that we do not collect, store, or share any personal information or data from users of our website or app.</p>
+      <p>If you have any questions or concerns about our privacy practices, please feel free to contact us.</p>
+      <p>By using our website and/or app, you acknowledge and agree to this Privacy Policy.</p>
+    </div>
+  )
+}
