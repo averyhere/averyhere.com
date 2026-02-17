a2d563cc554a963a17e88eb3eb02a399142a34f0
Avery Ondo <4927881+averyhere@users.noreply.github.com>
2026-02-17 12:04:37 -0700

    content update

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
           Web developer and marketer by profession, adventurer by passion.
         </p>
         <p>
-          With more than a decade of experience in web development, I have spent my career strategizing, building, scaling, and optimizing both the technical aspects of modern websites as well as the marketing focused web development, CRO, and analytics teams behind them. Having equal responsibilities as a leader, marketer, and technologist, I work in the intersection of creativity, analytics, and code.
+          With more than 15 years of experience in web development, I have spent my career strategizing, building, scaling, and optimizing both the technical aspects of modern websites as well as the marketing focused web development, CRO, and analytics teams behind them. Having equal responsibilities as a leader, marketer, and technologist, I work in the intersection of creativity, analytics, and code.
         </p>
         <p>
           When I'm not nerding out on web and marketing technologies you will likely find me hiking, biking, camping, or skiing in Colorado's beautiful Rocky Mountains.
