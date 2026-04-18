9535d7ca1df506871210adf41abab216dc14ddae
Avery Ondo <4927881+averyhere@users.noreply.github.com>
2026-04-18 13:28:23 -0600

    cleanup

--- src/app/(pages)/resources/dhmp-electrolysis-coverage-guide/page.tsx
+++ src/app/(pages)/resources/dhmp-electrolysis-coverage-guide/page.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
 import { PiArrowFatLineLeftDuotone } from "react-icons/pi";
 
 export const dynamic = "force-static";
@@ -13,13 +15,13 @@ export default function Page() {
   return (
     <>
       <aside>
-        <a
+        <Link
           href="/resources/"
           className="font-light decoration-blue underline-offset-4 decoration-2 hover:underline flex items-center gap-1 mb-4 mt-12"
         >
-          <PiArrowFatLineLeftDuotone className="inline-block" />
+          <PiArrowFatLineLeftDuotone className="inline-block text-bright-purple" />
           Back
-        </a>
+        </Link>
       </aside>
       <article>
         <header className="mb-4">
@@ -31,7 +33,7 @@ export default function Page() {
           </p>
         </header>
         <section>
-          <h2 className="mb-4 text-xl font-bold">Introduction & Disclaimer</h2>
+          <h2 className="my-4 text-xl font-bold">Introduction & Disclaimer</h2>
           <p className="text-sm font-light my-2">
             This guide is intended to help folks who have a Denver Health
             Elevate Medical Plan (DHMP) navigate the process for ensuring their
@@ -50,16 +52,63 @@ export default function Page() {
             well as the process for obtaining pre-authorization and
             reimbursement.
           </p>
-          <p>
-            This guide is not intended as legal or medical advice, but rather as
-            a resource to help navigate the process for these specific services.
-            Always consult with your healthcare provider and insurance company
-            for personalized guidance and information about your medical needs
-            and coverage.
+          <p className="text-sm font-light my-2">
+            <strong>
+              This guide is not intended as legal or medical advice
+            </strong>
+            , but rather as a resource to help navigate the process for these
+            specific services. Always consult with your healthcare provider and
+            insurance company for personalized guidance and information about
+            your medical needs and coverage.
           </p>
         </section>
         <section>
-          <h2 className="mb-4 text-xl font-bold">
+          <h2 className="my-4 text-xl font-bold">Overview of the Process</h2>
+          <div className="prose prose-sm dark:prose-invert">
+            <p>
+              The process for getting electrolysis and laser hair removal
+              covered by DHMP can be broken down into three main steps:
+            </p>
+            <ol>
+              <li>Verify your coverage</li>
+              <li>Get a pre-authorization</li>
+              <li>Requesting reimbursement</li>
+            </ol>
+            <p>
+              The following resources are provided by DHMP and will be
+              referenced throughout this guide:
+            </p>
+            <ul>
+              <li>
+                <a
+                  href="https://www.denverhealthmedicalplan.org/resource-library?audience=members&resource_type=member_handbooks_eocs"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  DHMP Resources Page
+                </a>
+              </li>
+              <li>
+                <Link
+                  href="/PriorAuthorizationRequestForm_2024_Eng.pdf"
+                  target="_blank"
+                >
+                  DHMP Pre-Authorization Form
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/DHMP-Medical-Reimbursement-Form-2026.pdf"
+                  target="_blank"
+                >
+                  DHMP Reimbursement Form
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section>
+          <h2 className="my-4 text-xl font-bold">
             Step 1: Check your coverage
           </h2>
           <div className="prose prose-sm dark:prose-invert">
@@ -67,23 +116,26 @@ export default function Page() {
               Before we get too deep, let's double-check your plan details to
               ensure Electrolysis is included in your health plan coverage. This
               is outlined in your member handbook. These handbooks are specific
-              to each individual health plan offered by DHMP and so it's
-              important to check the relevant one. To find the name of your
-              plan, refer to the top of your insurance card. You can find your
-              member handbook{" "}
-              <a href="https://www.denverhealthmedicalplan.org/resource-library?audience=members&resource_type=member_handbooks_eocs">
+              to each individual health plan offered by DHMP. It's important to
+              reference the relevant one. Refer to the top of your insurance
+              card for your specific plan name, then locate your member handbook{" "}
+              <a
+                href="https://www.denverhealthmedicalplan.org/resource-library?audience=members&resource_type=member_handbooks_eocs"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 here
               </a>
               .
             </p>
             <p>
-              Once you've found the correct handbook, locate the section titled
-              "Benefits/Coverage (What’s covered?)" section. Within this section
-              is a sub-section titled "Gender Dysphoria and Gender-Affirming
-              Care" with a list of services which are covered. If you see
-              "Electrolysis" or "Laser hair removal" listed, then you're good to
-              go! If not, you may want to reach out to DHMP to clarify whether
-              it's covered under a different name or if there are any specific
+              Within your handbook find the section titled "Benefits/Coverage
+              (What’s covered?)". Below that is a sub-section titled "Gender
+              Dysphoria and Gender-Affirming Care" with a list of services. If
+              you see "Electrolysis" or "Laser hair removal" listed (depending
+              on what you're looking to get covered), then you're good to go! If
+              not, you may want to reach out to DHMP to clarify whether it's
+              covered under a different name or if there are any specific
               requirements for coverage.
             </p>
           </div>
@@ -120,12 +172,12 @@ export default function Page() {
             <p>
               Contact your primary care provider and request they submit the
               pre-authorization form. You can download the pre-auth form{" "}
-              <a
+              <Link
                 href="/PriorAuthorizationRequestForm_2024_Eng.pdf"
                 target="_blank"
               >
                 here
-              </a>
+              </Link>
               . You will need to provide the following information to your
               provider to fill out the form:
             </p>
@@ -163,12 +215,12 @@ export default function Page() {
               you a receipt. All receipts will look different, and you need this
               in addition to the reimbursement form provided by DHMP which you
               can download{" "}
-              <a
+              <Link
                 href="/DHMP-Medical-Reimbursement-Form-2026.pdf"
                 target="_blank"
               >
                 here
-              </a>
+              </Link>
               .
             </p>
             <p>
@@ -200,17 +252,35 @@ export default function Page() {
               reimbursement and not specific to each service that may need
               reimbursement."
             </blockquote>
-            <strong>Services section:</strong> <br />
-            <p>In the "Services" section, list each session you had with:</p>
+            <strong>Services:</strong> <br />
+            <p>
+              You can include up to 4 individual sessions on one form, so you
+              can submit less forms if you had multiple sessions. There is a
+              time limit for submitting reimbursement requests, so be sure to
+              submit your forms promptly. According to DHMP requests must be
+              submitted within 180 days of the date of service. However, sending
+              them as soon as possible is recommended, especially if you had
+              multiple sessions, to ensure you don't miss the deadline for any
+              of them. Insurance companies also like reimbursing smaller
+              increments instead of large sums, something to keep in mind when
+              deciding how many sessions to include on each form.
+            </p>
+            <p>
+              In the "Services" section, list each session you had with all the
+              required information:
+            </p>
             <ul>
               <li>
-                Date of service: <em>(From and To will be the same)</em>
+                Date of service:{" "}
+                <em>(The From and To dates will be the same)</em>
               </li>
               <li>
-                Place of service: <em>(DHMP stated this should be "11")</em>
+                Place of service:{" "}
+                <em>(DHMP informed me this should be "11")</em>
               </li>
               <li>
-                Furnished via audio/video?: <em>(No)</em>
+                Furnished via audio/video?:{" "}
+                <em>(No, unless your provider is magical)</em>
               </li>
               <li>
                 CPT/HCPCS Code:{" "}
@@ -230,7 +300,8 @@ export default function Page() {
                 Charge per Unit/Day:{" "}
                 <em>
                   (Calculate how much each 30 minutes of service costs, for
-                  example if charged $100 for one hour this would be $50)
+                  example if total charge is $100 for one hour this would be
+                  $50)
                 </em>
               </li>
               <li>
@@ -249,23 +320,77 @@ export default function Page() {
               </li>
             </ul>
             <p>
-              You can include up to 4 individual sessions on one form, so you
-              can submit less forms if you had multiple sessions. Just be sure
-              to include all the necessary information for each session.
-            </p>
-            <p>
-              Once the form is filled out, you can submit it to DHMP along with
-              your receipts. You can submit these via mail. The address and
-              instructions for submission are included on the reimbursement
-              form. It typically takes a few weeks for DHMP to process the
-              reimbursement, and they will send you a check in the mail for the
-              reimbursed amount.
+              Once the form is filled out, signed, and dated, you can submit it
+              to DHMP along with your receipts. You must submit these via mail,
+              further instructions are included on the first page of the
+              reimbursement form. It typically takes a few weeks for DHMP to
+              process the reimbursement, and they will send you a check in the
+              mail for the reimbursed amount. You should be able to track the
+              status of your claim in the DHMP portal or by contacting DHMP
+              directly.
             </p>
             <p>
               If you have any issues with reimbursement, you can reach out to
               DHMP for clarification and assistance. Be sure to keep copies of
               all your forms and receipts for your records in case you need to
               reference them in the future.
+            </p>
+          </div>
+        </section>
+        <section>
+          <h2 className="my-4 text-xl font-bold">Helpful tips</h2>
+          <div className="prose prose-sm dark:prose-invert">
+            <ul>
+              <li>
+                Create a spreadsheet to keep track of appointments, the
+                information needed for the reimbursement form, and the status of
+                each reimbursement request. This helped me stay organized and
+                ensure I didn't miss any deadlines for submitting my
+                reimbursement requests.
+              </li>
+              <li>
+                Keep a copy of <em>everything</em> you submit to DHMP and any
+                correspondence you have with them. This will be helpful in case
+                you need to reference it later or if there are any issues with
+                your reimbursement requests. I kept a folder on my computer with
+                all my forms, receipts, and correspondence with DHMP organized
+                by date, which made it easy to find what I needed when I needed
+                it.
+              </li>
+              <li>
+                Fill out the form as completely as possible ahead of time on the
+                computer. Then just add in the date, charges, and units for each
+                session before submitting. This saved me a lot of time and
+                effort when it came to filling out the forms. It also allowed me
+                to print out several forms at once and update the remainder of
+                information by hand as needed (example below).
+                <figure>
+                  <Image
+                    src="/images/services-prefilled.png"
+                    alt="Example of reimbursement form filled out ahead of time on computer"
+                    width={1054}
+                    height={344}
+                  />
+                  <figcaption className="text-sm font-light text-center mt-2">
+                    Example of reimbursement form filled out ahead of time on
+                    computer
+                  </figcaption>
+                </figure>
+              </li>
+            </ul>
+          </div>
+          <h2 className="my-4 text-xl font-bold">Final thoughts</h2>
+          <div className="prose prose-sm dark:prose-invert">
+            <p>
+              Navigating insurance coverage for electrolysis and laser hair
+              removal can be a complex and sometimes frustrating process, but
+              with the right information and resources, it is possible to get
+              the coverage you need. I hope this guide has been helpful in
+              outlining the steps and providing some tips for navigating the
+              process with DHMP. Remember to always advocate for yourself and
+              don't hesitate to reach out to your insurance provider or
+              healthcare provider if you have any questions or need assistance
+              along the way.
             </p>
           </div>
         </section>
