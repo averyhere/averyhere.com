1cc21e998ad518ecef3421b48d25801c7dfbbf32
Avery Ondo <4927881+averyhere@users.noreply.github.com>
2026-04-18 13:38:19 -0600

    formatted

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,12 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/admin/','/login/','/login/mfa/','/error/'],
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/admin/", "/login/", "/login/mfa/", "/error/"],
     },
-    sitemap: 'https://averyhere.com/sitemap.xml',
-  }
+    sitemap: "https://averyhere.com/sitemap.xml",
+  };
 }
